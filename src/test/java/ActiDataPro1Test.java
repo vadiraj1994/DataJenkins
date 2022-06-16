f527acbc4dbe1807a374db406fc6b80bfcddc253
HP <HP@LAPTOP-DCD51ORS>
2022-06-16 16:30:51 +0530

    Data added

---
+++ src/test/java/ActiDataPro1Test.java
@@ -0,0 +1,9 @@
+
+
+public class ActiDataPro1Test
+{
+public static void main(String[] args) {
+	
+	System.out.println("Hi");
+}	
+}
---
+++ src/test/java/Data1.java
@@ -0,0 +1,9 @@
+
+public class Data1
+{
+public static void main(String[] args) {
+	
+	String data="one";
+	System.out.println(data);
+}
+}
